ebf37fcb2b427cd74d430e37af3e4c0998f12be7
HariomPC <pandya.hariom@gmail.com>
2022-08-05 20:27:49 +0530

    committed

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'om_hospital',
-    'version' : '1.0.0',
-    'summary': 'First Application',
-    'sequence': -100,
-    'description': """ First Application """,
-    'installable': True,
-    'auto_install':False,
-    'application':True,
-    'license': 'LGPL-3'
+    'name' : 'Om Hospital',    
+    'license': 'LGPL-3',
 }
