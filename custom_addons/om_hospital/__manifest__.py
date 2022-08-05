145d91a10fc1555bc23b5bae30827cc136046c4f
HariomPC <pandya.hariom@gmail.com>
2022-08-05 19:27:55 +0530

    created custom_addons folder and conf folder for configuration

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'om_hospital',
+    'version' : '1.0.0',
+    'summary': 'First Application',
+    'sequence': -100,
+    'description': """ First Application """,
+    'installable': True,
+    'auto_install':False,
+    'application':True,
+    'license': 'LGPL-3'
+}
