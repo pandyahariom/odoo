3a09df03a1d914aa92d362e106b48e59a814468f
pandyahariom <pandya.hariom@gmail.com>
2022-08-08 13:29:14 +0530

    related,date and datetime fields
    Gender field is automatic based on the selection of the customer
    added date and datetime fields

--- custom_addons/estate/models/customer_model.py
+++ custom_addons/estate/models/customer_model.py
@@ -4,3 +4,6 @@ class Customer(models.Model):
     _inherit='mail.thread','mail.activity.mixin'
     _description="Model to demonstrate Many2one concept"
     customer_id=fields.Many2one(comodel_name="estate.property", string="Customer Name")
+    gender=fields.Selection([('male','Male'),('female','Female')],string="Gender",related="customer_id.gender")
+    appointment_time=fields.Datetime(string="Appointment Time",default=fields.Datetime.now)
+    appointment_date=fields.Date(string="Appointment Date",default=fields.Date.today)
