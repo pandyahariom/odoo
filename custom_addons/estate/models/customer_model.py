cddac63b88484960d67979bdcfe5c47c90498bb6
pandyahariom <pandya.hariom@gmail.com>
2022-08-08 16:31:25 +0530

    Compute filed added not working

--- custom_addons/estate/models/customer_model.py
+++ custom_addons/estate/models/customer_model.py
@@ -1,4 +1,6 @@
 from odoo import models,fields
+from datetime import date
+
 class Customer(models.Model):
     _name="customer.details"
     _inherit='mail.thread','mail.activity.mixin'
@@ -6,4 +8,21 @@ class Customer(models.Model):
     customer_id=fields.Many2one(comodel_name="estate.property", string="Customer Name")
     gender=fields.Selection([('male','Male'),('female','Female')],string="Gender",related="customer_id.gender")
     appointment_time=fields.Datetime(string="Appointment Time",default=fields.Datetime.now)
-    appointment_date=fields.Date(string="Appointment Date",default=fields.Date.today)
+    birth_date=fields.Date(string="Birth Date",default=fields.Date.today)
+    age=fields.Integer(string="Age",compute="_compute_age")
+
+    def _compute_age(self):
+        today = date.today()
+        for record in self:
+            try:
+                birthday = record.birth_date.replace(year = today.year)
+            # raised when birth date is February 29
+            # and the current year is not a leap year
+            except ValueError:
+                birthday = record.birth_date.replace(year = today.year,
+                        month = record.birth_date.month + 1, day = 1)
+        
+            if birthday > today:
+                return today.year - record.birth_date.year - 1
+            else:
+                return today.year - record.birth_date.year
