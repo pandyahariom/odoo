c26f4d68eadbfd37e274c94788d986d5bbe71f7d
pandyahariom <pandya.hariom@gmail.com>
2022-08-08 11:59:44 +0530

    Tracking and SearchPanel
    1) Enabled tracking using boolean field of model
    2) Added SearchPanel in search view

--- custom_addons/estate/models/estate_model.py
+++ custom_addons/estate/models/estate_model.py
@@ -3,7 +3,7 @@ class Estate(models.Model):
     _name="estate.property"
     _inherit='mail.thread','mail.activity.mixin'
     _description="First Model to store Estate Properties"
-    name=fields.Char(string="CustomerName")
+    name=fields.Char(string="CustomerName",tracking=True)
     age=fields.Integer(string="Age")
-    gender=fields.Selection([('male','Male'),('female','Female')],string="Gender")
+    gender=fields.Selection([('male','Male'),('female','Female')],string="Gender",default="male")
     active=fields.Boolean(string="Active or Archive",default=True)
