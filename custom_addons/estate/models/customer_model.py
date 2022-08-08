4620f2ffe43f6645583e9d4fa6a52b9ac531e818
HariomPC <pandya.hariom@gmail.com>
2022-08-08 19:19:08 +0530

    onchange and depends
    1) Age calculation using depends
    2) name2 using onchange
    
    3) Difference between onchange and related:
    
    Editable related will change record of estate.model too

--- custom_addons/estate/models/customer_model.py
+++ custom_addons/estate/models/customer_model.py
@@ -1,5 +1,6 @@
-from odoo import models,fields
+from odoo import models,fields,api
 from datetime import date
+
 
 class Customer(models.Model):
     _name="customer.details"
@@ -9,20 +10,29 @@ class Customer(models.Model):
     gender=fields.Selection([('male','Male'),('female','Female')],string="Gender",related="customer_id.gender")
     appointment_time=fields.Datetime(string="Appointment Time",default=fields.Datetime.now)
     birth_date=fields.Date(string="Birth Date",default=fields.Date.today)
-    age=fields.Integer(string="Age",compute="_compute_age")
-
+    age=fields.Integer(string="Age",compute="_compute_age") #Demo of depends
+    
+    name2=fields.Char(string="OnChangeDemo") #Retrive the name while name is changed
+    @api.onchange('customer_id')
+    def onchange_customer_id(self):
+        self.name2=self.customer_id.name
+#Mentioning api.depends will update the Age as soon as user select birth_date. 
+# If not specified it will get updated on save and not on birth_date selection.
+    @api.depends('birth_date')
     def _compute_age(self):
         today = date.today()
         for record in self:
             try:
                 birthday = record.birth_date.replace(year = today.year)
+                #print("Brithdate:",birthday)
             # raised when birth date is February 29
             # and the current year is not a leap year
             except ValueError:
                 birthday = record.birth_date.replace(year = today.year,
                         month = record.birth_date.month + 1, day = 1)
-        
+                #print("Brithdate:",birthday)
+            #print("ANS:",today.year - record.birth_date.year)
             if birthday > today:
-                return today.year - record.birth_date.year - 1
+                self.age= today.year - record.birth_date.year - 1
             else:
-                return today.year - record.birth_date.year
+                self.age= today.year - record.birth_date.year
