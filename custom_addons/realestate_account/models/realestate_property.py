41048c15a416ff24de3f442f9533fb40e1f7641a
HariomPC <pandya.hariom@gmail.com>
2022-08-23 19:57:16 +0530

    With Invoice

--- custom_addons/realestate_account/models/realestate_property.py
+++ custom_addons/realestate_account/models/realestate_property.py
@@ -1,5 +1,36 @@
-from odoo import fields,api,models
+from odoo import fields,api,models,_
+from odoo.exceptions import UserError
+from odoo import Command
 class realestate(models.Model):
     _inherit="realestate.property"
     def action_sold(self):
-        return super().action_sold()
+        super_call=super().action_sold()
+
+        for property in self:
+            journal = self.env["account.move"].with_context(default_move_type="out_invoice")._get_default_journal()
+            new_invoice={
+                "partner_id": property.buyer_id.id,
+                "move_type": "out_invoice",
+                "journal_id": journal.id,
+                "invoice_line_ids": [
+                     Command.create(
+                        {
+                            "name": property.name,
+                            "quantity": 1.0,
+                            "price_unit": property.selling_price * 6.0 / 100.0,
+                        }
+                        ),
+                    Command.create(
+                        {
+                            "name": "Administrative Fees",
+                            "quantity": 1.0,
+                            "price_unit": 100.0,
+                        }
+                        )
+                    ]
+                        
+                }
+            self.env["account.move"].create(new_invoice) #Create need dictionary as argument
+        
+        return super_call
+       
